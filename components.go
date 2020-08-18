f19b8e2e7130cb00f4409fe804734d31e85c7fc6
Guy-PC\Guy <tbk.guy@gmail.com>
2020-08-19 00:05:25 +0300

    server support for multiple clients

--- components.go
+++ components.go
@@ -10,3 +10,9 @@ type peer struct {
 	id   uuid.UUID
 	addr net.UDPAddr
 }
+
+type peerPair struct {
+	id       uuid.UUID
+	internal net.UDPAddr
+	external net.UDPAddr
+}
--- server.go
+++ server.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"net"
+
+	"github.com/google/uuid"
 )
 
 func startServer() {
+	// Listen for clients registration
 	UDPListenAddr, err := net.ResolveUDPAddr("udp", ":5000")
 	conn, err := net.ListenUDP("udp", UDPListenAddr)
 	if err != nil {
@@ -13,30 +16,37 @@ func startServer() {
 		panic(err)
 	}
 
-	clients := &clients{registered: make(map[string]peer)}
+	peers := make(map[string]peerPair)
 	for {
 		buffer := make([]byte, 1024)
 		_, addr, err := conn.ReadFromUDP(buffer)
+
 		if err == nil { //message received succcessfully
-			connectingPeer := peer{}
-			if deserialize(buffer, &connectingPeer) == nil { //message format is correct
-				connectingPeer.ExternalAddr = *addr
-				go handleConnection(conn, clients, connectingPeer)
+			internalPeer := peer{}
+			if deserialize(buffer, &internalPeer) == nil { //message format is correct
+				go handleConnection(conn, peers, internalPeer, addr)
 			}
 		}
 		fmt.Println("Recieved message from: ", addr.String())
 	}
 }
 
-func handleConnection(serverSock *net.UDPConn, clients *clients, connectingPeer peer) {
-	connectingPeerAddr := connectingPeer.ExternalAddr.String()
-	if _, ok := clients.registered[connectingPeerAddr]; !ok {
-		clients.registered[connectingPeerAddr] = connectingPeer // if is useless (might save lookup, or perhaps adds readability)
+func handleConnection(serverConn *net.UDPConn, peers map[string]peerPair, internal peer, connectingAddr *net.UDPAddr) {
+	connectingPeerAddr := connectingAddr.String()
+
+	if _, ok := peers[connectingPeerAddr]; !ok {
+		id := uuid.New()
+		internal.id = id
+		peers[connectingPeerAddr] = peerPair{id: id, internal: internal.addr, external: *connectingAddr}
 	}
 
-	for address := range clients.registered {
+	for address, pair := range peers {
 		if address != connectingPeerAddr {
-			serverSock.WriteToUDP(serialize(clients.registered[address]), &connectingPeer.ExternalAddr) // send any client which is not the connecting one
+			externalPeerInfo := NetworkMessage{messageType: update, payload: serialize(peer{id: pair.id, addr: pair.external})}
+			internalPeerInfo := NetworkMessage{messageType: update, payload: serialize(peer{id: pair.id, addr: pair.internal})}
+
+			serverConn.WriteToUDP(serialize(externalPeerInfo), connectingAddr)
+			serverConn.WriteToUDP(serialize(internalPeerInfo), connectingAddr) // send any client which is not the connecting one
 		}
 	}
 }
